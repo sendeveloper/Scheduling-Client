6a451a9b49bd654892b0e4aaeb86dd2df1e8d8b3
A senior web developer <lightmorning46@gmail.com>
2018-10-01 15:31:14 -0700

    Update server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const express = require('express');
 const app = express();
 
 // Serve static files
-app.use(express.static(__dirname + '/dist/school-scheduling'));
+app.use(express.static(__dirname + '/dist/'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/school-scheduling/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku port
